6bf3ca2c4d5e15e952edda14885f3858b1ae80ab
kieranvenison <venison@hey.com>
2024-02-10 12:35:49 +0000

    fix: placeholder

--- src/app/components/LogExerciseForm/LogExerciseForm.tsx
+++ src/app/components/LogExerciseForm/LogExerciseForm.tsx
@@ -93,7 +93,7 @@ const LogExerciseForm = ({ exercise, close }: LogExerciseFormProps) => {
                     <Field
                       type="number"
                       name="log"
-                      placeHolder="100kg"
+                      placeholder="100kg"
                       className="text-sm rounded block w-full p-2.5 bg-zinc-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     />
                     <ErrorMessage
@@ -110,7 +110,7 @@ const LogExerciseForm = ({ exercise, close }: LogExerciseFormProps) => {
                     <Field
                       type="date"
                       name="date"
-                      placeHolder="dd/mm/yyyy"
+                      placeholder="dd/mm/yyyy"
                       className="text-sm rounded block w-full p-2.5 bg-zinc-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 h-[42px]"
                     />
                     <ErrorMessage
