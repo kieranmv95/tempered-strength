89b698a03627feb57ee4fd93d1ff3cca263d53e6
kieranvenison <venison@hey.com>
2024-02-12 11:00:54 +0000

    feat: Better header height to show CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       {/* Magic number is the header height*/}
-      <div className="relative h-[calc(100vh-74.48px)] flex justify-center items-center">
+      <div className="relative h-[500px] md:h-[750px] lg:h-[875px] flex justify-center items-center">
         <Image
           className="absolute top-0 left-0 w-full h-full object-cover z-0 opacity-20"
           src="/deadlift.jpg"
@@ -16,7 +16,7 @@ export default function Home() {
           width={2400}
           height={1600}
         />
-        <div className="text-[1.25rem] font-black leading-none pb-12 md:text-[2rem] xl:text-[3rem] z-10 text-center">
+        <div className="text-[1.25rem] font-black leading-none pb-4 md:text-[2rem] xl:text-[3rem] z-10 text-center">
           <p className="text-[3em]">LIFT IT</p>
           <p className="text-[2.07em]">TRACK IT</p>
           <p className="text-[2.1em]">SHARE IT</p>
@@ -38,6 +38,9 @@ export default function Home() {
         />
         <div className="relative z-10">
           <h2 className="text-2xl font-bold lg:text-4xl">Sign up for FREE</h2>
+          <p className="mt-3">
+            Be tracking and sharing your lifts in a matter of minutes
+          </p>
           <div className="px-4 md:w-full max-w-[650px] md:mx-auto">
             <Link
               href="/sign-up"
