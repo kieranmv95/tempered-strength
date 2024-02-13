a9824be3246769d8c66154fe57b0afd4db34e0d3
kieranvenison <venison@hey.com>
2024-02-13 15:56:33 +0000

    feat: Update user

---
+++ src/app/account/page.tsx
@@ -0,0 +1,11 @@
+import UpdateUserDetails from "@/app/account/UpdateUserDetails";
+
+export default function Account() {
+  return (
+    <div className="px-4 py-12 container mx-auto">
+      <h1 className="text-2xl font-bold lg:text-4xl mb-3">Account</h1>
+      <p className="mb-6">Click the field to update/change the value</p>
+      <UpdateUserDetails />
+    </div>
+  );
+}
---
+++ src/types/IUser.ts
@@ -0,0 +1,5 @@
+export type IUser = {
+  id: string;
+  username: string;
+  onboarding: number;
+};
