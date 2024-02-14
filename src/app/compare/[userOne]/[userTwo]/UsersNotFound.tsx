e492019fcebea660746106d515fff8fdf81f7c7d
kieranvenison <venison@hey.com>
2024-02-14 18:50:59 +0000

    refactor: split out components

---
+++ src/app/compare/[userOne]/[userTwo]/UsersNotFound.tsx
@@ -0,0 +1,14 @@
+import BackButton from "@/app/components/BackButton";
+
+const UsersNotFound = () => (
+  <div className="text-center">
+    <div className="inline-block mx-auto mt-12">
+      <BackButton href="/compare">Back to compare</BackButton>
+    </div>
+    <h1 className="text-2xl">
+      Users not found or users have not logged any lifts yet!
+    </h1>
+  </div>
+);
+
+export default UsersNotFound;
