22709141182520d6a48e8c9b8912c84c8c648356
kieranvenison <venison@hey.com>
2024-03-05 19:53:39 +0000

    refactor: removed close dep

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
     document.addEventListener('keyup', handleKeyUp);
 
     return () => document.removeEventListener('keyup', handleKeyUp);
-  }, [close]);
+  }, []);
 
   return (
     <>
