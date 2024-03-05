9022d1aa1a35d47c45fa80ff3450d67c4421563d
kieranvenison <venison@hey.com>
2024-03-05 20:00:58 +0000

    feat: navIcon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,8 @@ import {
   UnauthenticatedHeader,
 } from '@/components/Header/Navigation';
 import { SignedIn, SignedOut, useAuth } from '@clerk/nextjs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   const { isSignedIn } = useAuth();
@@ -55,14 +57,11 @@ const Header = () => {
               className="flex items-center px-3 py-2 border rounded text-amber-400 border-amber-400 hover:text-amber-100 hover:border-amber-100"
               onClick={() => setNavOpen(!navOpen)}
             >
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
+              {navOpen ? (
+                <FontAwesomeIcon icon={faX} className="h-3 w-3" />
+              ) : (
+                <FontAwesomeIcon icon={faBars} className="h-3 w-3" />
+              )}
             </button>
           </div>
           <div
